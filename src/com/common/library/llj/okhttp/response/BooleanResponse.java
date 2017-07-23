d187aac90fd6a3ebc569fbd7ff73a0e74053314d
liulinjie <liulinjie1990823@gmail.com>
2017-07-23 12:42:14 +0800

    增加返回类型基类

---
+++ src/com/common/library/llj/okhttp/response/BooleanResponse.java
@@ -0,0 +1,12 @@
+package com.common.library.llj.okhttp.response;
+
+import com.common.library.llj.base.BaseResponse;
+
+/**
+ * project:babyphoto_app
+ * describe:
+ * Created by llj on 2017/7/23.
+ */
+
+public class BooleanResponse extends BaseResponse<Boolean> {
+}
